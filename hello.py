3cff6d02e73bb5273face94ceb8cea8ffbd15947
Zain0189 <zainshahid089@gmail.com>
2025-07-04 08:45:24 +0500

    Fix typo in greeting message. Closes #2

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Zain! welcome to Flask!"
+    return "Hello User! welcome to Flask!"
 
 if __name__ == "__main__":
     app.run(debug=True)
